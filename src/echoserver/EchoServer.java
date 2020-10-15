c0bfce0a23520debb9be8688f467a8c0c3fb7655
Jonah-Northwood <north305@morris.umn.edu>
2020-10-15 10:10:24 -0500

    Add multiple solutions but none work

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -2,6 +2,7 @@ package echoserver;
 import java.net.*;
 import java.io.*;
 
+/*
 public class EchoServer {
     public static final int portNumber = 6013;
 
@@ -14,15 +15,16 @@ public class EchoServer {
       while (true) {
         // Wait until someone connects
         Socket client = sock.accept();
-
+        System.out.println("Got a request!");
        InputStream input = client.getInputStream();
-        int b = input.read();
+        
 
         OutputStream output = client.getOutputStream();
 
         // Send the current input back to the client.
+        int inputByte = input.read();
         output.flush();
-        output.write(b);
+        output.write(inputByte);
 
         // Close the client socket since we're done.
         input.close();
@@ -36,3 +38,80 @@ public class EchoServer {
     }
   }
 }
+*/
+
+
+public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = sock.accept();
+        System.out.println("Got a request!");
+
+        // Construct a writer so we can write to the socket, thereby
+        // sending something back to the client.
+        InputStream input = client.getInputStream();
+
+        // Send the current date back tothe client.
+        OutputStream output = client.getOutputStream();
+        int inputByte;
+        int cab;
+        while ((inputByte =System.in.read()) != -1) {
+          output.flush();
+          output.write(inputByte);
+        }
+
+
+        // Close the client socket since we're done.
+        client.close();
+      }
+    // *Very* minimal error handling.
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
+}
+
+
+/*
+public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = sock.accept();
+        System.out.println("Got a request!");
+
+        InputStream input = client.getInputStream();
+        // Construct a writer so we can write to the socket, thereby
+        // sending something back to the client.
+        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+        // Send the current date back tothe client.
+        writer.println(input);
+
+        // Close the client socket since we're done.
+        client.close();
+      }
+    // *Very* minimal error handling.
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
+}
+*/
